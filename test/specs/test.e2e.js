c10d20b25c6ac41e0cf6232fb2ef327174804e50
Hrqlv <henriquelopesvelozo@gmail.com>
2024-08-07 16:06:06 -0300

    Feito a estrutura para os tests e ajustes nas config

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,10 @@
+// const { expect } = require('@wdio/globals')
+// const LoginPage = require('../pageobjects/login.page')
+// const SecurePage = require('../pageobjects/secure.page')
+
+describe('Sample', () => {
+    it('Sample test', async () => {
+        await driver.pause(5000)
+    })
+})
+
